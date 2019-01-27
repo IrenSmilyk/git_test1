9712509961ede5405b93c9c9a352dd517e0b2638
IrenSmilyk <smilykirina@gmail.com>
2019-01-27 10:16:32 +0200

    com6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,10 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        System.out.println("Input hello: ");
+        System.out.println("Input hello: ");
+        System.out.println("Input hello: ");
+        System.out.println("Input hello: ");
         System.out.println("Input hello: ");
 
 
