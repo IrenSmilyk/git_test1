abda11ff00ec81966ca33661b96d676755ce8cbc
IrenSmilyk <smilykirina@gmail.com>
2019-01-27 09:07:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input value: ");
+        String split = scan.nextLine();
+
+        // String[] split = "123 234 678".split("\\s+");
+       //System.out.println(Arrays.toString(split));
+        //System.out.println(Integer.parseInt(split[0]) + 2);
+    }
+}
