8883dcc714572bc01a5cb351a19feea9beafe23a
IrenSmilyk <smilykirina@gmail.com>
2019-01-27 09:31:07 +0200

    cjm4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,17 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-        System.out.println("Input value: ");
-       
+        System.out.println("Input hello: ");
+
+
 
         // String[] split = "123 234 678".split("\\s+");
        //System.out.println(Arrays.toString(split));
